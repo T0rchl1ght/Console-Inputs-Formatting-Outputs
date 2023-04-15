6386f6855847d7182c32a835d050f889926cd828
T0rchl1ght <122705226+T0rchl1ght@users.noreply.github.com>
2023-04-15 14:12:38 -0700

    Add files via upload

---
+++ grocalc.py
@@ -0,0 +1,44 @@
+
+foods_prompt = """
+Which food are you purchasing (Choose a letter)?
+A) Banana
+B) Apple
+C) Cheese
+D) Pickles
+Q) To Quit
+"""
+
+while True:
+    qty_prompt = """
+How many do you need(Enter a number?)
+"""
+
+    food = input(foods_prompt)
+    if food == 'Q':
+        print('Goodbye! Come again')
+        break
+    
+    if food not in ['A', 'B', 'C', 'D']:
+        print('Invalid Option... Try again')
+        continue
+    else:
+        quantity = int(input(qty_prompt))
+        if quantity < 0:
+            print('Quantity wrong')
+            continue
+        
+        if food == 'A':
+            cost = 0.30
+            food_label = 'Banana'
+        elif food == 'B':
+            cost = 1.00
+            food_label = 'Apple'
+        elif food == 'C':
+            cost = 3.00
+            food_label = 'Cheese' 
+        else:
+            cost = 2.99
+            food_label = 'Pickles'
+        total = cost * quantity 
+        result = "You are buying {} {} for ${}".format(quantity, food, total)
+        print(result)
